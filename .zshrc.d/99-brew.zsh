697330c1bfcbad668483ca67ddc30685f52c1b48
Martin Meredith <martin@sourceguru.net>
2018-11-28 18:21:32 +0000

    Move mac stuff to mac repo

--- .zshrc.d/99-brew.zsh
+++
@@ -1,4 +0,0 @@
-if [[ "$KERNEL" == "Darwin" ]]; then
-    alias brewup='brew update; brew upgrade; brew cask upgrade; brew prune; brew cleanup; brew doctor'
-    export PATH=/usr/local/sbin:$PATH
-fi
--- .zshrc.d/99-gnu-path.zsh
+++
@@ -1,7 +0,0 @@
-if [[ "$KERNEL" == "Darwin" ]]; then
-    export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
-    export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
-
-    alias ls="gls --color=always"
-fi
-
